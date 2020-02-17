ae8a4f1520ffa293f79fcbd35ab105482d4b2a55
Lionel Tzatzkin <elrumordelaluz@hotmail.com>
2020-02-17 12:54:49 +0100

    Update docs

---
+++ stories/Modals/SideEffectsModal.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+export default function SideEffectsModal({ closeModal, focusRef, side, ...rest }: {
+    [x: string]: any;
+    closeModal: any;
+    focusRef: any;
+    side: any;
+}): JSX.Element;
--- stories/Modals/index.d.ts
+++ stories/Modals/index.d.ts
@@ -1,3 +1,4 @@
 export { default as StaticModal } from './StaticModal';
 export { default as ExtraPropsModal } from './ExtraPropsModal';
 export { default as ParagraphsModal } from './ParagraphsModal';
+export { default as SideEffectsModal } from './SideEffectsModal';
